7d59435c81b5d8203fba0ab74cc6331624c831dd
Anna Saweljew <anna.saweljew@o2.pl>
2022-02-26 09:46:06 +0100

    initial commit

---
+++ src/test/java/pl/sages/egzamin/EgzaminApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sages.egzamin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgzaminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
